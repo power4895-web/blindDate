438f3245aa31fcc014140e4e7af6db5e18e0f4f9
sunwooKo <power4895@naver.com>
2023-08-08 18:16:01 +0900

    커밋2

--- src/main/java/com/example/self_board_project/main/MainController.java
+++ src/main/java/com/example/self_board_project/main/MainController.java
@@ -1,16 +1,14 @@
 package com.example.self_board_project.main;
 
-import com.example.self_board_project.core.authority.Auth;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-    @RequestMapping(value="/")
-    public String main (Auth auth)
-    {
-        return "main";
-    }
+//    @RequestMapping(value="/")
+//    public String main (Auth auth)
+//    {
+//        return "main";
+//    }
 
 
 
