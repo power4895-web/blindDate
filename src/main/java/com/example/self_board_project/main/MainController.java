f496be7b7e079bfcddea5bc44b35a4a0261209d0
sunwooKo <power4895@naver.com>
2023-08-08 18:21:43 +0900

    main수정2

--- src/main/java/com/example/self_board_project/main/MainController.java
+++ src/main/java/com/example/self_board_project/main/MainController.java
@@ -1,12 +1,13 @@
 package com.example.self_board_project.main;
 
+import com.example.self_board_project.core.authority.Auth;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
     @RequestMapping(value="/")
-    public static String main ()
+    public static String main (Auth auth)
     {
         return "main";
     }
